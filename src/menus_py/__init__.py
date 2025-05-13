24f3400881e250eb55ad93da3b6156471e6282ed
Kai <kai.fautley@gmail.com>
2025-05-13 11:41:36 +0100

    1.3.0 - untested

--- src/menus_py/__init__.py
+++ src/menus_py/__init__.py
@@ -1,6 +1,7 @@
 # menus.py
 # Simple menu-related functions with input validation
 # Kai Fautley
+import warnings as _w
 
 # printed usage documentation is stored in a dictionary structure
 docs = {
@@ -18,9 +19,13 @@ docs = {
                     "return_name":"Returns the name of the option selected",
                     "return_index":"Returns the index of the option in the list of options"
                 }
+        },"simpleChoice":{
+            "syntax":"simpleChoice(question,letters,first_only=True)"
         },"yesNo":{
             "syntax":"yesNo(question)",
-                "summary":"Asks the user for a yes/no answer to a question",
+                "summary":"Calls simpleChoice(question, [\"y\",\"n\"]). Deprecated as of 1.3.0 as\n"
+                "simpleChoice supersedes it, though remains present (and functionally identical)\n"
+                "for compatibility reasons.",
                 "usage":{
                     "question":"The question to be asked"
                 }
@@ -34,23 +39,26 @@ class InvalidMenuException(Exception):
         self.message = message
         super().__init__(self.message)
 
-def mainMenu(title,options,return_name=False,return_index=True):
+def mainMenu(title,options,return_name=False,return_index=True,layout="  {}. {}"): # layout defaults to match pre-1.3.0 behaviour
     # Full text-based menu, gives a question and asks a user to select from a series of options. Handles most unexpected inputs.
 
     # check the options list is actually a list and throw an error if not
-    if type(options) != list:
+    if type(options) is not list:
         raise InvalidMenuException(f"Options is of type {type(options).__name__}, not list!") 
     # similar check for the title being a string
-    elif type(title) != str:
+    elif type(title) is not str:
         raise InvalidMenuException(f"Title is of type {type(title).__name__}, not str!")
     # check there are actually options to choose from
     elif options == []:
         raise InvalidMenuException("Options cannot be empty")
+    # check that the layout is valid
+    if layout.count("{}") != 2:
+        raise InvalidMenuException("Invalid layout, too little or too many {}'s'")
     
     # create the printed text for the menu
     menu_text = f"{title}\n\n" # title and newlines
     for option in range(len(options)): # for each option, include the option number and name
-        menu_text += f"  {option+1}. {options[option]}\n" # TODO: make layout customisable
+        menu_text += layout.format(option+1, options[option])+"\n" # use layout to define the style of the options - new in 1.3.0
     menu_text += f"\nEnter an option [1-{option+1}]: " # Prompt for the user
     
     valid = False
@@ -76,29 +84,47 @@ def mainMenu(title,options,return_name=False,return_index=True):
         choice -= 1
     return choice # return the choice as an integer for the program to deal with
 
-def yesNo(question):
-    # TODO: rewrite to accept different sets of letters (under another name) then convert this function to just link to that.
-    
-    # Keeps asking a question until Yes or No is entered
-    # borrows some code from `menu`, more detailed comments are there
+def simpleChoice(question,letters,first_only=True):
+    # Basic choice of single-letter options - replaces core for yesNo()
+    # New in 1.3.0
 
     # Make sure the question is a string
-    if type(question) != str:
+    if type(question) is not str:
         raise InvalidMenuException(f"Question is of type {type(question).__name__}, not str!")
+    # check letter list is valid
+    letters2 = []
+    for letter in letters:
+        if len(letter) > 1: # This only allows single-letter options
+            raise InvalidMenuException(f"Option '{letter}' is too long!")
+        if letter.upper() in letters2: # Two occurences of a letter can be confusing
+            raise InvalidMenuException(f"Option '{letter}' appears more than once!")
+        letters2.append(letter.upper()) # This both ensures the above check works and creates a list of all-capitalised letters
+    if letters == []:
+        raise InvalidMenuException("List of options cannot be empty!") # Ensures the list actually has options in it
     
-    menu_text = f"{question} [y/n]: "
+    menu_text = f"{question} [{[f"{letters[letter]}/" for i in range(letters-1)]}{letters[-1]}]: " # Formats the menu text. It's a bit of a mess, I'll clean it up later.
+
     valid = False
     while not valid: # keep going until we get something actually useful out of the user
         # actual user interaction starts here
         choice = input(menu_text).strip()
         try: # an exception will be raised if the input is empty, so use a try block to catch that
-            if choice[0].lower() in ["y","n"]: # check if the first letter is "y" or "n", to catch if the user enters "Yes" or "No"
+            if first_only:
+                choice = choice[0] # only check the first letter so any unabbreviated forms (e.g. "y" for "yes") will work.
+            else:
+                choice[0] # try anyway to make sure "Answer cannot be empty!" is triggered
+            if choice.upper() in letters2:
                 valid = True
             else:
-                input(f"{choice} is not Y or N.")
+                input(f"{choice} is not an option.")
         except ValueError:
             input("Answer cannot be empty!")
     return choice[0].lower() # return the formatted result
+
+def yesNo(question):
+    # Deprecated as of 1.3.0
+    _w.warn(DeprecationWarning("This function is deprecated and may be removed in a future version of the module."))
+    return simpleChoice(question,["y","n"])
 
 
 if __name__ == "__main__":
